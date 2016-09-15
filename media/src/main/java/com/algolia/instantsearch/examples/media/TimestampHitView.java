614b48295fa433291676d5906326bf04adcc9ff8
Paul-Louis NECH <pln@algolia.com>
2016-09-15 16:13:33 +0200

    Media: display timestamp using RelativeTimeTextView

---
+++ media/src/main/java/com/algolia/instantsearch/examples/media/TimestampHitView.java
@@ -0,0 +1,22 @@
+package com.algolia.instantsearch.examples.media;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.algolia.instantsearch.views.AlgoliaHitView;
+import com.github.curioustechizen.ago.RelativeTimeTextView;
+
+import org.json.JSONObject;
+
+
+public class TimestampHitView extends RelativeTimeTextView implements AlgoliaHitView {
+    public TimestampHitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onUpdateView(JSONObject result) {
+        String timestamp = result.optString("published");
+        setReferenceTime(Long.parseLong(timestamp) * 1000 /* milliseconds timestamp required */);
+    }
+}
