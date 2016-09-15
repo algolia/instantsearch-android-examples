41fec92d44471dec2ccdba00a4c1c19300a14b8c
Paul-Louis NECH <pln@algolia.com>
2016-09-15 15:34:04 +0200

    Media: Log ErrorEvents

--- media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
+++ media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
@@ -2,11 +2,16 @@ package com.algolia.instantsearch.examples.media;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 
 import com.algolia.instantsearch.InstantSearchHelper;
 import com.algolia.instantsearch.Searcher;
+import com.algolia.instantsearch.events.ErrorEvent;
 import com.algolia.search.saas.Client;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EventBus.getDefault().register(this);
     }
 
     @Override
@@ -26,5 +32,10 @@ public class MainActivity extends AppCompatActivity {
         new InstantSearchHelper(this, menu, R.id.action_search,
                 new Searcher(new Client(ALGOLIA_APP_ID, ALGOLIA_API_KEY).initIndex(ALGOLIA_INDEX_NAME)));
         return true;
+    }
+
+    @Subscribe
+    public void onErrorEvent(ErrorEvent event) {
+        Log.e("PLN", "Error searching" + event.query.getQuery() + ":" + event.error.getLocalizedMessage());
     }
 }
