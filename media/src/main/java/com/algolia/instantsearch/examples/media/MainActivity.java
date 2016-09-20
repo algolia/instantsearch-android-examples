1fa6a0093ba802a2df303b0bc0cac8b2783bcc7b
Paul-Louis NECH <pln@algolia.com>
2016-09-20 20:02:19 +0200

    Media: Automatic facet filter bounds

--- media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
+++ media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
@@ -23,18 +23,25 @@ public class MainActivity extends AppCompatActivity {
     private static final String ALGOLIA_API_KEY = "0c9899197c49f80b183adc0f68ea8d78";
 
     private Searcher searcher;
+    private FilterResultsFragment filterResultsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EventBus.getDefault().register(this);
+        searcher = new Searcher(new Client(ALGOLIA_APP_ID, ALGOLIA_API_KEY).initIndex(ALGOLIA_INDEX_NAME));
+        filterResultsFragment = new FilterResultsFragment().with(this, searcher)
+                .addSeekBar("views", 100)
+                .addSeekBar("rating", "stars", 100)
+                .addCheckBox("cc", "Closed Captions (CC)", true) //TODO: Can we disable a checkbox when results say empty
+                .addCheckBox("4k", "4K", true)
+                .addCheckBox("hd", "HD", true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        searcher = new Searcher(new Client(ALGOLIA_APP_ID, ALGOLIA_API_KEY).initIndex(ALGOLIA_INDEX_NAME));
         new InstantSearchHelper(this, menu, R.id.action_search, searcher);
         searcher.search(); //Show results for empty query on startup
         menu.findItem(R.id.action_search).expandActionView(); //open SearchBar on startup
@@ -46,13 +53,7 @@ public class MainActivity extends AppCompatActivity {
         if (item.getItemId() == R.id.action_filter) {
             final FragmentManager fragmentManager = getSupportFragmentManager();
             if (fragmentManager.findFragmentByTag(FilterResultsFragment.TAG) == null) {
-                new FilterResultsFragment().with(this, searcher)
-                        .addSeekBar("views", 0, 1000000000, 100)
-                        .addSeekBar("rating", "stars", 0, 5, 100)
-                        .addCheckBox("cc", "Closed Captions (CC)", true)
-                        .addCheckBox("4k", "4K", true)
-                        .addCheckBox("hd", "HD", true)
-                        .show(fragmentManager, FilterResultsFragment.TAG);
+                filterResultsFragment.show(fragmentManager, FilterResultsFragment.TAG);
             }
             return true;
         }
