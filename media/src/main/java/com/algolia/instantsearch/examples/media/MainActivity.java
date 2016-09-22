5a2cbf17ae3a6a1d55c5d5fd08e71de21f2e826e
Paul-Louis NECH <pln@algolia.com>
2016-09-22 17:29:42 +0200

    Media: Use FilterResultsFragment#get to instanciate fragment

--- media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
+++ media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
@@ -26,9 +26,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String ALGOLIA_API_KEY = "0c9899197c49f80b183adc0f68ea8d78";
 
     private Searcher searcher;
+
     private FilterResultsFragment filterResultsFragment;
     private SearchView searchView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +37,8 @@ public class MainActivity extends AppCompatActivity {
         // Initialize a Searcher with your credentials and an index name
         searcher = new Searcher(new Client(ALGOLIA_APP_ID, ALGOLIA_API_KEY).initIndex(ALGOLIA_INDEX_NAME));
         searcher.search(); //Show results for empty query on startup
-
         // Create the FilterResultsFragment here so it can set the appropriate facets on the Searcher
-        filterResultsFragment = new FilterResultsFragment().with(searcher)
+        filterResultsFragment = FilterResultsFragment.get(searcher)
                 .addSeekBar("views", 100)
                 .addSeekBar("rating", "stars", 100)
                 .addCheckBox("cc", "Closed Captions (CC)", true)
