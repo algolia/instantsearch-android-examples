2939b221565c1456fce982a9e8f9802eb3770514
Paul-Louis NECH <pln@algolia.com>
2016-09-26 20:05:31 +0200

    Ecommerce: Hide secondary attributes when null

---
+++ ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/NotNullView.java
@@ -0,0 +1,29 @@
+package com.algolia.instantsearch.examples.ecommerce;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class NotNullView extends TextView {
+    public NotNullView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NotNullView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public static boolean isNull(CharSequence text) {
+        return text == null || text.equals("") || text.equals("null");
+    }
+
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        if (isNull(text)) {
+            setVisibility(GONE);
+        } else {
+            setVisibility(VISIBLE);
+            super.setText(text, type);
+        }
+    }
+}
--- ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/WrappedNotNullView.java
+++ ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/WrappedNotNullView.java
@@ -6,31 +6,31 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 
 
-public class WrappedTextView extends TextView {
+public class WrappedNotNullView extends NotNullView {
     private String prefix;
     private String suffix;
     private CharSequence text;
 
-    public WrappedTextView(Context context, AttributeSet attrs) {
+    public WrappedNotNullView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        final TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.WrappedTextView);
+        final TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.WrappedNotNullView);
         setAttributes(styledAttributes);
     }
 
-    public WrappedTextView(Context context, AttributeSet attrs, int defStyle) {
+    public WrappedNotNullView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
 
-        final TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.WrappedTextView, defStyle, 0);
+        final TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.WrappedNotNullView, defStyle, 0);
         setAttributes(styledAttributes);
     }
 
     private void setAttributes(TypedArray styledAttributes) {
-        prefix = styledAttributes.getString(R.styleable.WrappedTextView_prefix);
+        prefix = styledAttributes.getString(R.styleable.WrappedNotNullView_prefix);
         if (prefix == null) {
             prefix = "";
         }
-        suffix = styledAttributes.getString(R.styleable.WrappedTextView_suffix);
+        suffix = styledAttributes.getString(R.styleable.WrappedNotNullView_suffix);
         if (suffix == null) {
             suffix = "";
         }
@@ -38,8 +38,12 @@ public class WrappedTextView extends TextView {
     }
 
     @Override
-    public void setText(CharSequence text, BufferType type) {
-        super.setText(text != null && !text.equals("") ? prefix + text + suffix : text, type);
+    public void setText(CharSequence text, TextView.BufferType type) {
+        if (isNull(text)) {
+            super.setText(text, type);
+        } else {
+            super.setText(prefix + text + suffix, type);
+        }
         this.text = text;
     }
 
@@ -52,4 +56,5 @@ public class WrappedTextView extends TextView {
         this.suffix = suffix;
         setText(text);
     }
+
 }
