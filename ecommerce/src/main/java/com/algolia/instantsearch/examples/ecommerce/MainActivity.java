b5a5c70e00b62c2a792958e790b2a6e220fd5cb2
Paul-Louis NECH <pln@algolia.com>
2016-09-27 19:32:46 +0200

    Ecommerce: MVP version of dropdown filters

--- ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/MainActivity.java
+++ ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/MainActivity.java
@@ -2,9 +2,11 @@ package com.algolia.instantsearch.examples.ecommerce;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.algolia.instantsearch.InstantSearchHelper;
 import com.algolia.instantsearch.Searcher;
+import com.algolia.instantsearch.ui.FilterResultsWindow;
 import com.algolia.instantsearch.views.SearchBox;
 import com.algolia.search.saas.Client;
 
@@ -20,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Searcher searcher = new Searcher(new Client(ALGOLIA_APP_ID, ALGOLIA_API_KEY).initIndex(ALGOLIA_INDEX_NAME))
                 .search(); // Show results for empty query on app launch
-        new InstantSearchHelper(this, searcher);
+        new InstantSearchHelper(this, searcher); // Initialize InstantSearch in this activity with searcher
 
         ((SearchBox) findViewById(R.id.searchBox)).disableFullScreen(); // disable fullscreen input UI on landscape
+
+        new FilterResultsWindow(this, searcher)
+                .addSeekBar("salePrice", "price", 0d, 10000d, 100) //TODO: Accept int/float as well
+                .setToggleButton((Button) findViewById(R.id.btn_filter));
     }
 }
