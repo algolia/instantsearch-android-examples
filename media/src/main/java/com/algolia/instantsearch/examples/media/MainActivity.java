ca856ccea0b64163722cb17de3412d674e216971
Paul-Louis NECH <pln@algolia.com>
2016-10-21 16:09:35 +0200

    Media: Fix SearchView import

--- media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
+++ media/src/main/java/com/algolia/instantsearch/examples/media/MainActivity.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.algolia.instantsearch.events.ErrorEvent;
