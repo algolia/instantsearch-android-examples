8ca9634666588c96a313bcd3ccb199c31959c736
Paul-Louis NECH <pln@algolia.com>
2017-12-20 15:04:12 +0100

    feat(movies): First draft of multi index demo

---
+++ movies/src/main/java/com/algolia/instantsearch/examples/movies/MoviesActivity.java
@@ -0,0 +1,162 @@
+package com.algolia.instantsearch.examples.movies;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.algolia.instantsearch.helpers.InstantSearch;
+import com.algolia.instantsearch.helpers.Searcher;
+
+public class MoviesActivity extends AppCompatActivity {
+
+    private static final String ALGOLIA_APP_ID = "latency";
+    private static final String ALGOLIA_INDEX_ACTORS = "actors";
+    private static final String ALGOLIA_INDEX_MOVIES = "movies";
+    private static final String ALGOLIA_API_KEY = "d0a23086ed4be550f70be98c0acf7d74";
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private Searcher searcherActors;
+    private Searcher searcherMovies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Initialize a Searcher with your credentials and an index name
+        searcherActors = Searcher.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_ACTORS);
+//        searcherMovies = Searcher.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_MOVIES);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_movies, menu);
+
+        new InstantSearch(this, menu, R.id.action_search, searcherActors);// link the Searcher to the UI
+//        new InstantSearch(this, menu, R.id.action_search, searcherMovies);// link the Searcher to the UI
+        searcherActors.search(getIntent()); // Show results for empty query (on app launch) / voice query (from intent)
+//        searcherMovies.search(getIntent()); // Show results for empty query (on app launch) / voice query (from intent)
+
+        final MenuItem itemSearch = menu.findItem(R.id.action_search);
+        itemSearch.expandActionView(); //open SearchBar on startup
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        searcherMovies.destroy();
+        searcherActors.destroy();
+        super.onDestroy();
+    }
+
+    public static abstract class LayoutFragment extends Fragment {
+        private final int layout;
+
+        public LayoutFragment(@LayoutRes int layout) {
+            this.layout = layout;
+        }
+
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(layout, container, false);
+        }
+    }
+
+    public static class MoviesFragment extends LayoutFragment {
+        public MoviesFragment() {
+            super(R.layout.fragment_movies);
+        }
+    }
+
+    public static class ActorsFragment extends LayoutFragment {
+        public ActorsFragment() {
+            super(R.layout.fragment_actors);
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            switch (position) {
+                case 0:
+                    return new MoviesFragment();
+                default:
+                    return new ActorsFragment();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+}
