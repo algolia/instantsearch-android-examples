b1021443cfea347d1ef8f0008721df21a4940a2b
Paul-Louis NECH <pln@algolia.com>
2018-05-02 15:28:01 +0200

    refactor(QS): Move credentials to BuildConfig

--- querysuggestions/src/main/java/com/algolia/instantsearch/examples/querysuggestions/QSContentProvider.kt
+++ querysuggestions/src/main/java/com/algolia/instantsearch/examples/querysuggestions/QSContentProvider.kt
@@ -1,10 +1,6 @@
 package com.algolia.instantsearch.examples.querysuggestions
 
-//TODO: Move to e.g. BuildConfig
-import android.util.Log
-import com.algolia.instantsearch.examples.querysuggestions.QuerySuggestionsActivity.Companion.ALGOLIA_API_KEY
-import com.algolia.instantsearch.examples.querysuggestions.QuerySuggestionsActivity.Companion.ALGOLIA_APP_ID
-import com.algolia.instantsearch.examples.querysuggestions.QuerySuggestionsActivity.Companion.ALGOLIA_INDEX_SUGGESTIONS
+import com.algolia.instantsearch.examples.querysuggestions.BuildConfig.*
 import com.algolia.instantsearch.helpers.QuerySuggestionsContentProvider
 import com.algolia.instantsearch.helpers.Searcher
 import com.algolia.search.saas.Index
@@ -15,7 +11,6 @@ class QSContentProvider : QuerySuggestionsContentProvider() {
     }
 
     override fun getIndex(): Index? {
-        Log.d("QS_CP", "Creating index.")
         return Searcher.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_SUGGESTIONS, "suggestions").index
     }
 }
--- querysuggestions/src/main/java/com/algolia/instantsearch/examples/querysuggestions/QuerySuggestionsActivity.kt
+++ querysuggestions/src/main/java/com/algolia/instantsearch/examples/querysuggestions/QuerySuggestionsActivity.kt
@@ -3,20 +3,13 @@ package com.algolia.instantsearch.examples.querysuggestions
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import com.algolia.instantsearch.examples.querysuggestions.BuildConfig.*
 import com.algolia.instantsearch.helpers.InstantSearch
 import com.algolia.instantsearch.helpers.Searcher
 import com.algolia.instantsearch.model.SearchBoxViewModel
 import com.algolia.instantsearch.ui.views.SearchBox
 
 class QuerySuggestionsActivity : AppCompatActivity() {
-    companion object {
-        internal const val ALGOLIA_APP_ID = "latency"
-        internal const val ALGOLIA_INDEX_PRODUCTS = "instant_search"
-        internal const val ALGOLIA_INDEX_SUGGESTIONS = "instantsearch_query_suggestions"
-        internal const val ALGOLIA_API_KEY = "afc3dd66dd1293e2e2736a5a51b05c0a"
-    }
-
     private lateinit var searcherProducts: Searcher
 
     override fun onCreate(savedInstanceState: Bundle?) {
