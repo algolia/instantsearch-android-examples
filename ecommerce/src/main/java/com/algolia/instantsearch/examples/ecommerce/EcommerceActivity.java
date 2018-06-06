c5dbe8e44af0414bec32848e251964f2118577b9
Paul-Louis NECH <pln@algolia.com>
2018-06-06 15:29:04 +0200

    refactor(PermissionRationale): Just SnackBar, no Toast

--- ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
+++ ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
@@ -1,5 +1,6 @@
 package com.algolia.instantsearch.examples.ecommerce;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -13,6 +14,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
