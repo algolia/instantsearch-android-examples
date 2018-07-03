eff688adddeea94d373a97cda1856ed8a36f5ded
Paul-Louis NECH <pln@algolia.com>
2018-07-03 16:31:12 +0200

    WIP: Offline compatibility exploration

--- ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
+++ ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
@@ -3,13 +3,19 @@ package com.algolia.instantsearch.examples.ecommerce;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.algolia.instantsearch.helpers.InstantSearch;
 import com.algolia.instantsearch.helpers.Searcher;
 import com.algolia.instantsearch.ui.views.SearchBox;
+import com.algolia.search.saas.MirroredIndex;
+import com.algolia.search.saas.OfflineClient;
+import com.algolia.search.saas.Query;
+import com.algolia.search.saas.SyncListener;
 import com.squareup.leakcanary.RefWatcher;
 
 public class EcommerceActivity extends AppCompatActivity {
@@ -29,34 +35,59 @@ public class EcommerceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecommerce);
 
-        searcher = Searcher.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME);
-        new InstantSearch(this, searcher); // Initialize InstantSearch in this activity with searcher
-        searcher.search(getIntent()); // Show results for empty query (on app launch) / voice query (from intent)
-
-        ((SearchBox) findViewById(R.id.searchBox)).disableFullScreen(); // disable fullscreen input UI on landscape
-        filterResultsWindow = new FilterResultsWindow.Builder(this, searcher)
-                .addSeekBar("salePrice", "initial price", 100)
-                .addSeekBar("customerReviewCount", "reviews", 100)
-                .addCheckBox("promoted", "Has a discount", true)
-                .addSeekBar("promoPrice", "price with discount", 100)
-                .build();
-
-        buttonFilter = findViewById(R.id.btn_filter);
-        buttonFilter.setOnClickListener(new View.OnClickListener() {
+        final String TAG = "OFFLINE";
+        OfflineClient client = new OfflineClient(this, ALGOLIA_APP_ID, "7f3c958936c885de959cb4c55e45751e");
+        client.enableOfflineMode("AisEAQH/qZqmBf/s9qQFABpwYXVsbG91aXMubmVjaEBhbGdvbGlhLmNvbQEqMC4CFQCN+t2zKoat7gw5KD81N/kwXrnO/gIVAMRcrN9UBivanjEXUohjJWNRDGbl");
+        final MirroredIndex test = client.getIndex(ALGOLIA_INDEX_NAME);
+        test.setMirrored(true);
+        test.setOfflineFallbackTimeout(1);
+        test.addDataSelectionQuery(new MirroredIndex.DataSelectionQuery(new Query("iPhone"), 2));
+        test.addSyncListener(new SyncListener() {
             @Override
-            public void onClick(View v) {
-                final boolean willDisplay = !filterResultsWindow.isShowing();
-                if (willDisplay) {
-                    filterResultsWindow.showAsDropDown(buttonFilter);
-                } else {
-                    filterResultsWindow.dismiss();
-                }
-                toggleArrow(buttonFilter, willDisplay);
+            public void syncDidStart(MirroredIndex index) {
+                Log.e(TAG, "Syncing");
+            }
+
+            @Override
+            public void syncDidFinish(MirroredIndex index, Throwable error, MirroredIndex.SyncStats stats) {
+                searcher = Searcher.create(test);
+                new InstantSearch(EcommerceActivity.this, searcher); // Initialize InstantSearch in this activity with searcher
+                searcher.search(getIntent()); // Show results for empty query (on app launch) / voice query (from intent)
+
+                ((SearchBox) findViewById(R.id.searchBox)).disableFullScreen(); // disable fullscreen input UI on landscape
+                filterResultsWindow = new FilterResultsWindow.Builder(EcommerceActivity.this, searcher)
+                        .addSeekBar("salePrice", "initial price", 100)
+                        .addSeekBar("customerReviewCount", "reviews", 100)
+                        .addCheckBox("promoted", "Has a discount", true)
+                        .addSeekBar("promoPrice", "price with discount", 100)
+                        .build();
+
+                buttonFilter = findViewById(R.id.btn_filter);
+                buttonFilter.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final boolean willDisplay = !filterResultsWindow.isShowing();
+                        if (willDisplay) {
+                            filterResultsWindow.showAsDropDown(buttonFilter);
+                        } else {
+                            filterResultsWindow.dismiss();
+                        }
+                        toggleArrow(buttonFilter, willDisplay);
+                    }
+                });
+            }
+        });
+        new Handler(getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                test.sync();
+
             }
         });
     }
 
-    @Override protected void onNewIntent(Intent intent) {
+    @Override
+    protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         searcher.search(intent);
     }
