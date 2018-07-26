8cf0d2fa96a14e88ef9726ffa61feecf25edadaa
Paul-Louis NECH <1821404+PLNech@users.noreply.github.com>
2018-07-26 12:05:39 +0200

    feat(Ecommerce): Voice ui via lib (#12)
    * feat(Ecommerce): Permission request workflow
    
    * feat(Ecommerce): Voice search button
    
    * chore(Ecommerce): Disable system voice search
    
    * fixup! feat(Ecommerce): Permission request workflow
    
    * chore: Update kotlin, gradle (1.2.41,3.2.0-a15)
    
    * feat(Ecommerce): Voice overlay layout and display
    
    * feat(VoiceDialogFragment): Handle voice recognition
    
    * feat(VoiceDialogFragment): Trigger search
    
    * style(voice_overlay): Improve UI
    
    * fix(VoiceDialog): avoid NPE, stop reco on error
    
    * refactor(PermissionRationale): Just SnackBar, no Toast
    
    * refactor(VoiceDialogFragment): Better layout
    
    * feat(VoiceDialogFragment): Start listening onResume
    
    * refactor(MainActivity): Rename SnackBar
    
    * style(EcommerceActivity): Reorganize methods
    
    * nit(build.gradle): Remove productFlavors
    
    * refactor(EcommerceActivity): Clean code, remove debug Logs
    
    * refactor(VoiceDialogFragment): Clean code, remove debug Logs
    
    * refactor(ProgressBar): set color in code for compat <21
    
    * fix(VoiceDialogFragment): destroy recognizer when stopping, stop before dismiss
    
    * fix(VoiceDialogFragment): color progressBar
    
    * refactor(style): Extract styles and attributes
    
    * refactor(EcommerceActivity): VoiceUI from library
    
    * chore(build.gradle): Update constraint library
    
    * chore: Update kotlin/constraint-layout
    
    * refactor(ecommerce): use instantsearch-voice
    
    * refactor: Use PDG's setTitle
    
    * chore: Add buildscript dependencies
    
    * chore: Update gradle plugin to latest
    
    * chore(gradle): Update wrapper to 4.8
    
    * chore(gradle): Update wrapper to 4.9-rc1
    
    * chore(build.gradle): Remove useless comment
    
    * style(EcommerceActivity): Add Nullability annotations
    
    * chore: Use released voice v1

--- ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
+++ ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
@@ -1,19 +1,36 @@
 package com.algolia.instantsearch.examples.ecommerce;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BaseTransientBottomBar;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.algolia.instantsearch.helpers.InstantSearch;
 import com.algolia.instantsearch.helpers.Searcher;
 import com.algolia.instantsearch.ui.views.SearchBox;
+import com.algolia.instantsearch.voice.PermissionDialogFragment;
+import com.algolia.instantsearch.voice.VoiceDialogFragment;
 import com.squareup.leakcanary.RefWatcher;
 
-public class EcommerceActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import static android.Manifest.permission.RECORD_AUDIO;
+import static com.algolia.instantsearch.voice.PermissionDialogFragment.ID_REQ_VOICE_PERM;
+
+public class EcommerceActivity extends AppCompatActivity implements VoiceDialogFragment.VoiceResultsListener {
     private static final String ALGOLIA_APP_ID = "latency";
     private static final String ALGOLIA_INDEX_NAME = "bestbuy_promo";
     private static final String ALGOLIA_API_KEY = "91e5b0d48d0ea9c1eb7e7e063d5c7750";
@@ -24,8 +41,9 @@ public class EcommerceActivity extends AppCompatActivity {
     private Button buttonFilter;
     private Searcher searcher;
 
+    // region Lifecycle
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecommerce);
 
@@ -56,7 +74,8 @@ public class EcommerceActivity extends AppCompatActivity {
         });
     }
 
-    @Override protected void onNewIntent(Intent intent) {
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
         super.onNewIntent(intent);
         searcher.search(intent);
     }
@@ -78,22 +97,105 @@ public class EcommerceActivity extends AppCompatActivity {
         refWatcher.watch(this);
         refWatcher.watch(findViewById(R.id.hits));
     }
+    // endregion
 
-    private void toggleArrow(Button b, boolean up) {
-        final Drawable[] currentDrawables = b.getCompoundDrawables();
-        final Drawable newDrawable;
-        if (up) {
-            if (arrowUp == null) {
-                arrowUp = getResources().getDrawable(R.drawable.arrow_up_flat);
-            }
-            newDrawable = arrowUp;
-        } else {
-            if (arrowDown == null) {
-                arrowDown = getResources().getDrawable(R.drawable.arrow_down_flat);
-            }
-            newDrawable = arrowDown;
+    // region Permission handling
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (isRecordPermissionWithResults(requestCode, grantResults)) {
+            if (isPermissionGranted(grantResults)) showVoiceOverlay();
+            else if (shouldExplainPermission()) showPermissionRationale();
+            else showPermissionManualInstructions();
         }
-        b.setCompoundDrawablesWithIntrinsicBounds(currentDrawables[0], currentDrawables[1], newDrawable, currentDrawables[3]);
-
     }
+
+    private void showPermissionRationale() {
+        Snackbar.make(findViewById(R.id.mic), R.string.voice_search_rationale, Snackbar.LENGTH_LONG)
+                .setAction(R.string.voice_search_button_again, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ActivityCompat.requestPermissions(EcommerceActivity.this, new String[]{RECORD_AUDIO}, ID_REQ_VOICE_PERM);
+                    }
+                }).show();
+    }
+
+    private void showPermissionManualInstructions() {
+        final View micView = findViewById(R.id.mic);
+        Snackbar snackbar = Snackbar.make(micView, R.string.voice_search_disabled_rationale, Snackbar.LENGTH_LONG)
+                .setAction(R.string.voice_search_button_enable, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Snackbar.make(micView, R.string.voice_search_disabled_instructions, Snackbar.LENGTH_SHORT)
+                                .addCallback(new BaseTransientBottomBar.BaseCallback<Snackbar>() {
+                                    @Override
+                                    public void onDismissed(Snackbar transientBottomBar, int event) {
+                                        startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                                .setData(Uri.fromParts("package", getPackageName(), null)));
+                                    }
+                                }).show();
+                    }
+                });
+        ((TextView) snackbar.getView().findViewById(android.support.design.R.id.snackbar_text)).setMaxLines(2);
+        snackbar.show();
+    }
+    // endregion
+
+    // region UI
+    private void toggleArrow(Button b, boolean isUp) {
+        final Drawable[] currentDrawables = b.getCompoundDrawables();
+        b.setCompoundDrawablesWithIntrinsicBounds(currentDrawables[0], currentDrawables[1], getArrowDrawable(isUp), currentDrawables[3]);
+    }
+
+    private Drawable getArrowDrawable(boolean isUp) {
+        if (isUp) arrowUp = ContextCompat.getDrawable(this, R.drawable.arrow_up_flat);
+        else arrowDown = ContextCompat.getDrawable(this, R.drawable.arrow_down_flat);
+        return isUp ? arrowUp : arrowDown;
+    }
+
+    public void onTapMic(@NonNull View view) {
+        if (hasRecordPermission()) {
+            showPermissionOverlay();
+        } else {
+            showVoiceOverlay();
+        }
+    }
+
+    private void showPermissionOverlay() {
+        PermissionDialogFragment frag = new PermissionDialogFragment();
+        frag.setTitle("You can use voice search to find products");
+        frag.show(getSupportFragmentManager(), "permission");
+    }
+
+    private void showVoiceOverlay() {
+        VoiceDialogFragment frag = new VoiceDialogFragment();
+        frag.setSuggestions("iPhone case", "Running shoes"); //TODO: use query suggestions?
+        frag.setVoiceResultsListener(this);
+        frag.show(getSupportFragmentManager(), "voice");
+    }
+
+    @Override
+    public void onVoiceResults(@NonNull ArrayList<String> matches) {
+        searcher.search(matches.get(0));
+    }
+    // endregion
+
+    // region Helpers
+    //TODO: Move helpers to Voice lib?
+    private boolean isPermissionGranted(int[] grantResult) {
+        return grantResult[0] == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean isRecordPermissionWithResults(int requestCode, @NonNull int[] grantResults) {
+        return requestCode == ID_REQ_VOICE_PERM && grantResults.length > 0;
+    }
+
+    private boolean hasRecordPermission() {
+        return ContextCompat.checkSelfPermission(getBaseContext(), RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    private boolean shouldExplainPermission() {
+        return ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO);
+    }
+    // endregion
 }
