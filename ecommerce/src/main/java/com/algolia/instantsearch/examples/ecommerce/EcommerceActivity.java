469a17d791ec3d7c7451359b92298f19028d6f54
Paul-Louis NECH <pln@algolia.com>
2018-07-26 12:03:48 +0200

    chore: Use released voice v1

--- ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
+++ ecommerce/src/main/java/com/algolia/instantsearch/examples/ecommerce/EcommerceActivity.java
@@ -12,7 +12,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BaseTransientBottomBar;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
