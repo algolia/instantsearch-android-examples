c8c6d0d6f374b993e300a82f878ea79a6693bf7d
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-05-13 11:01:33 +0200

    feat(relatedItems): use lib 2.4.0-beta02

--- showcase/src/main/kotlin/com/algolia/instantsearch/showcase/relateditems/RelatedItemsShowcase.kt
+++ showcase/src/main/kotlin/com/algolia/instantsearch/showcase/relateditems/RelatedItemsShowcase.kt
@@ -2,22 +2,19 @@ package com.algolia.instantsearch.showcase.relateditems
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.algolia.instantsearch.core.connection.Connection
 import com.algolia.instantsearch.core.connection.ConnectionHandler
 import com.algolia.instantsearch.core.hits.connectHitsView
+import com.algolia.instantsearch.helper.relateditems.MatchingPattern
+import com.algolia.instantsearch.helper.relateditems.connectRelatedHitsView
 import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
 import com.algolia.instantsearch.showcase.R
 import com.algolia.instantsearch.showcase.configureRecyclerView
 import com.algolia.instantsearch.showcase.configureSearcher
 import com.algolia.instantsearch.showcase.configureToolbar
-import com.algolia.instantsearch.showcase.relateditems.api.MatchingPattern
-import com.algolia.instantsearch.showcase.relateditems.api.connectHitsView2
 import com.algolia.instantsearch.showcase.stubIndex
 import com.algolia.search.helper.deserialize
 import com.algolia.search.model.Attribute
-import kotlinx.android.synthetic.main.showcase_relateditems.hits
-import kotlinx.android.synthetic.main.showcase_relateditems.relatedItems
-import kotlinx.android.synthetic.main.showcase_relateditems.toolbar
+import kotlinx.android.synthetic.main.showcase_relateditems.*
 
 class RelatedItemsShowcase : AppCompatActivity() {
 
@@ -35,21 +32,19 @@ class RelatedItemsShowcase : AppCompatActivity() {
         configureSearcher(relatedItemsSearcher)
 
         val hitsAdapter = ProductAdapter()
+        configureRecyclerView(hits, hitsAdapter)
         connection += searcher.connectHitsView(hitsAdapter) { response ->
             response.hits.deserialize(Product.serializer())
         }
-        configureRecyclerView(hits, hitsAdapter)
 
         val relatedItemsAdapter = ProductAdapter()
         configureRecyclerView(relatedItems, relatedItemsAdapter)
-
         val matchingPatterns: List<MatchingPattern<Product>> = listOf(
             MatchingPattern(Attribute("brand"), 1, Product::brand),
             MatchingPattern(Attribute("categories"), 2, Product::categories)
         )
-
         hitsAdapter.callback = { product ->
-            connection += relatedItemsSearcher.connectHitsView2(relatedItemsAdapter, product, matchingPatterns) { response ->
+            connection += relatedItemsSearcher.connectRelatedHitsView(relatedItemsAdapter, product, matchingPatterns) { response ->
                 response.hits.deserialize(Product.serializer())
             }
             relatedItemsSearcher.searchAsync()
--- showcase/src/main/kotlin/com/algolia/instantsearch/showcase/relateditems/api/internal/extensions/Fix.kt
+++
@@ -1,8 +0,0 @@
-package com.algolia.instantsearch.showcase.relateditems.api.internal.extensions
-
-// TODO(): Should be done by passing a param for example to FilterGroupsConverter.Legacy.Facet.
-internal fun List<List<String>>.unquote(): List<List<String>> {
-    return map { innerList ->
-        innerList.map { it.replace("\"", "") }
-    }
-}
