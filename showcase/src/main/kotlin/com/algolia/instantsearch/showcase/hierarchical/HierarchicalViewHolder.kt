e85844d023b0da2ae1250e7d5c1279584d928e33
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-07-21 12:05:53 +0200

    feat(hierarchical): make selected item bold

--- showcase/src/main/kotlin/com/algolia/instantsearch/showcase/hierarchical/HierarchicalViewHolder.kt
+++ showcase/src/main/kotlin/com/algolia/instantsearch/showcase/hierarchical/HierarchicalViewHolder.kt
@@ -1,9 +1,10 @@
 package com.algolia.instantsearch.showcase.hierarchical
 
+import android.graphics.Typeface
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.algolia.instantsearch.showcase.dip
 import com.algolia.instantsearch.helper.hierarchical.HierarchicalItem
+import com.algolia.instantsearch.showcase.dip
 import kotlinx.android.synthetic.main.list_item_selectable.view.*
 
 class HierarchicalViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
@@ -14,6 +15,16 @@ class HierarchicalViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         view.selectableItemSubtitle.text = item.facet.count.toString()
         view.selectableItemSubtitle.visibility = View.VISIBLE
         view.setOnClickListener(onClick)
-        view.selectableItemIcon.visibility = if (item.isSelected) View.VISIBLE else View.INVISIBLE
+        if (item.isSelected) selected() else unselected()
+    }
+
+    private fun selected() {
+        view.selectableItemIcon.visibility = View.VISIBLE
+        view.selectableItemName.setTypeface(null, Typeface.BOLD)
+    }
+
+    private fun unselected() {
+        view.selectableItemIcon.visibility = View.INVISIBLE
+        view.selectableItemName.setTypeface(null, Typeface.NORMAL)
     }
 }
