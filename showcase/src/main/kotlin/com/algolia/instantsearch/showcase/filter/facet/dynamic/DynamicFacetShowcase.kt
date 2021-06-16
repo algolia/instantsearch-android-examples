f408512299eac2d7ca2b20639a3ce0fc66333a94
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-06-16 16:18:55 +0200

    facets ordering showcase

--- showcase/src/main/kotlin/com/algolia/instantsearch/showcase/filter/facet/dynamic/DynamicFacetShowcase.kt
+++ showcase/src/main/kotlin/com/algolia/instantsearch/showcase/filter/facet/dynamic/DynamicFacetShowcase.kt
@@ -3,6 +3,7 @@ package com.algolia.instantsearch.showcase.filter.facet.dynamic
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.algolia.instantsearch.core.connection.ConnectionHandler
+import com.algolia.instantsearch.core.selectable.list.SelectionMode
 import com.algolia.instantsearch.helper.android.searchbox.SearchBoxViewAppCompat
 import com.algolia.instantsearch.helper.filter.facet.dynamic.DynamicFacetViewModel
 import com.algolia.instantsearch.helper.filter.facet.dynamic.connectFilterState
@@ -12,19 +13,45 @@ import com.algolia.instantsearch.helper.filter.state.FilterState
 import com.algolia.instantsearch.helper.searchbox.SearchBoxConnector
 import com.algolia.instantsearch.helper.searchbox.connectView
 import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
-import com.algolia.instantsearch.showcase.*
+import com.algolia.instantsearch.showcase.R
+import com.algolia.instantsearch.showcase.configureRecyclerView
+import com.algolia.instantsearch.showcase.configureSearchView
+import com.algolia.instantsearch.showcase.configureToolbar
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.configuration.ConfigurationSearch
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
 import com.algolia.search.model.Attribute
 import com.algolia.search.model.IndexName
+import io.ktor.client.features.logging.*
 import kotlinx.android.synthetic.main.include_search.*
 import kotlinx.android.synthetic.main.showcase_facet_list_search.*
 
 class DynamicFacetShowcase : AppCompatActivity() {
 
-    private val index = client.initIndex(IndexName("stub"))
+    private val client = ClientSearch(
+            ConfigurationSearch(
+                    ApplicationID("RVURKQXRHU"),
+                    APIKey("937e4e6ec422ff69fe89b569dba30180"),
+                    logLevel = LogLevel.ALL
+            )
+    )
+    private val index = client.initIndex(IndexName("test_facet_ordering"))
     private val searcher = SearcherSingleIndex(index)
     private val filterState = FilterState()
     private val searchBox = SearchBoxConnector(searcher)
-    private val dynamicFacetViewModel = DynamicFacetViewModel(emptyList(), mutableMapOf())
+
+    private val color = Attribute("color")
+    private val country = Attribute("country")
+    private val brand = Attribute("brand")
+    private val size = Attribute("size")
+
+    private val dynamicFacetViewModel = DynamicFacetViewModel(
+            selectionModeForAttribute = mapOf(
+                    color to SelectionMode.Multiple,
+                    country to SelectionMode.Multiple
+            )
+    )
     private val connection = ConnectionHandler(searchBox)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,11 +67,10 @@ class DynamicFacetShowcase : AppCompatActivity() {
         connection += dynamicFacetViewModel.connectView(adapter)
 
         configureToolbar(toolbar)
-        configureSearcher(searcher)
         configureSearchView(searchView, getString(R.string.search_brands))
         configureRecyclerView(hits, adapter)
 
-        searcher.query.facets = setOf(Attribute("brand"), Attribute("color"), Attribute("size"), Attribute("country"))
+        searcher.query.facets = setOf(brand, color, size, country)
         searcher.searchAsync()
     }
 
--- showcase/src/main/kotlin/com/algolia/instantsearch/showcase/filter/facet/dynamic/FacetItem.kt
+++ showcase/src/main/kotlin/com/algolia/instantsearch/showcase/filter/facet/dynamic/FacetItem.kt
@@ -5,5 +5,5 @@ import com.algolia.search.model.search.Facet
 
 sealed class FacetItem {
     data class Header(val attribute: Attribute) : FacetItem()
-    data class Value(val attribute: Attribute, val facet: Facet) : FacetItem()
+    data class Value(val attribute: Attribute, val facet: Facet, val selected: Boolean) : FacetItem()
 }
