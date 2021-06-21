37aba7b6a225935eaad6ffd67ced1353ceb61b68
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-06-21 11:12:06 +0200

    example: do not trigger search on empty query

--- showcase/src/main/kotlin/com/algolia/instantsearch/showcase/list/paging/PagingSingleIndexShowcase.kt
+++ showcase/src/main/kotlin/com/algolia/instantsearch/showcase/list/paging/PagingSingleIndexShowcase.kt
@@ -6,22 +6,32 @@ import androidx.lifecycle.Observer
 import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
 import com.algolia.instantsearch.core.connection.ConnectionHandler
-import com.algolia.instantsearch.showcase.*
-import com.algolia.instantsearch.showcase.list.movie.Movie
-import com.algolia.instantsearch.showcase.list.movie.MovieAdapterPaged
 import com.algolia.instantsearch.helper.android.list.SearcherSingleIndexDataSource
 import com.algolia.instantsearch.helper.android.searchbox.SearchBoxConnectorPagedList
 import com.algolia.instantsearch.helper.android.searchbox.SearchBoxViewAppCompat
 import com.algolia.instantsearch.helper.android.searchbox.connectView
 import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
-import kotlinx.android.synthetic.main.showcase_paging.*
+import com.algolia.instantsearch.showcase.R
+import com.algolia.instantsearch.showcase.configureRecyclerView
+import com.algolia.instantsearch.showcase.configureSearchView
+import com.algolia.instantsearch.showcase.configureSearcher
+import com.algolia.instantsearch.showcase.configureToolbar
+import com.algolia.instantsearch.showcase.list.movie.Movie
+import com.algolia.instantsearch.showcase.list.movie.MovieAdapterPaged
+import com.algolia.instantsearch.showcase.onResponseChangedThenUpdateNbHits
+import com.algolia.instantsearch.showcase.stubIndex
 import kotlinx.android.synthetic.main.include_search.*
+import kotlinx.android.synthetic.main.showcase_paging.*
 
 
 class PagingSingleIndexShowcase : AppCompatActivity() {
 
     private val searcher = SearcherSingleIndex(stubIndex)
-    private val dataSourceFactory = SearcherSingleIndexDataSource.Factory(searcher) { it.deserialize(Movie.serializer()) }
+    private val dataSourceFactory = SearcherSingleIndexDataSource.Factory(
+            searcher = searcher,
+            triggerSearchForQuery = { query -> !query.query.isNullOrEmpty() },
+            transformer = { it.deserialize(Movie.serializer()) }
+    )
     private val pagedListConfig = PagedList.Config.Builder().setPageSize(10).setEnablePlaceholders(false).build()
     private val movies = LivePagedListBuilder(dataSourceFactory, pagedListConfig).build()
     private val searchBox = SearchBoxConnectorPagedList(searcher, listOf(movies))
